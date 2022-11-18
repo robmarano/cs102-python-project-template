aa55a3d1aaf8d733baa95092f43f60da451d5538
Rob Marano <robmarano@gmail.com>
2022-11-18 13:24:36 -0500

    Initial commit.

---
+++ program.py
@@ -0,0 +1,12 @@
+#
+# program.py
+#
+# Author: Prof Rob Marano <rob@cooper.edu>
+
+def main():
+    # write your mainline logic here, add other functions if needed.
+    return
+
+# Python's mainline for your program
+if __name__ == '__main__':
+    main()
